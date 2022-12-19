180db20507a88220fe4d04ac1c297a4e2bb88923
Vladag <Vladag@DESKTOP-RHFQLAS>
2022-12-19 22:22:39 +0300

    First step

---
+++ src/main/java/telran/spring/stockmarket/StockMarketDataAnalystApplication.java
@@ -0,0 +1,13 @@
+package telran.spring.stockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketDataAnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketDataAnalystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/spring/stockmarket/StockMarketDataAnalystApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.spring.stockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketDataAnalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
