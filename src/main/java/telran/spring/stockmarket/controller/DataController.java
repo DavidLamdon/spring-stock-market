34a6160f940e4b1858cddc02c83daefbaa5b32cb
Vladag <Vladag@DESKTOP-RHFQLAS>
2022-12-24 15:13:40 +0300

    First Steps

--- src/main/java/telran/spring/stockmarket/controller/DataController.java
+++ src/main/java/telran/spring/stockmarket/controller/DataController.java
@@ -7,6 +7,30 @@ import telran.spring.stockmarket.service.DataService;
 
 public class DataController {
 	DataService dataService;
+	
+	//пример из сайта
+//	@PostMapping("excel")
+//	public String excelReader(@RequestParam("file") MultipartFile excel) {
+//		
+//		try {
+//			XSSFWorkbook workbook = new XSSFWorkbook(excel.getInputStream());
+//			XSSFSheet sheet = workbook.getSheetAt(0);
+//			
+//			for(int i=0; i<sheet.getPhysicalNumberOfRows();i++) {
+//				XSSFRow row = sheet.getRow(i);
+//				for(int j=0;j<row.getPhysicalNumberOfCells();j++) {
+//					System.out.print(row.getCell(j) +" ");
+//				}
+//				System.out.println("");
+//			}
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		return "Success";
+//	}
 
 	public DataDto addDataDto(DataDto dataDto) {
 		return dataService.addDataDto(dataDto);
